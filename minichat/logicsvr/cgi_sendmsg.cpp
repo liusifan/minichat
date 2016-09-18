0e5a65867f396992fdd3b67bed14edc184ab87ba
liusifan <gz_stephen@qq.com>
2016-09-19 03:37:49 +0800

    integrate account

--- minichat/logicsvr/cgi_sendmsg.cpp
+++ minichat/logicsvr/cgi_sendmsg.cpp
@@ -5,15 +5,61 @@
 
 #include "msgbox/msgbox_client.h"
 #include "seq/seq_client.h"
+#include "account/account_client.h"
 
 CgiSendMsg :: CgiSendMsg()
 {
     MsgBoxClient::Init( "~/minichat/etc/client/msgbox_client.conf" );
     SeqClient::Init( "~/minichat/etc/client/seq_client.conf" );
+    AccountClient::Init( "~/minichat/etc/client/account_client.conf" );
 }
 
 CgiSendMsg :: ~CgiSendMsg()
 {
+}
+
+static int AddOneMsg( const logic::ReqHead & head, const logic::MsgRequest & msg,
+        msgbox::AddMsgResp * result )
+{
+    int ret = 0;
+
+    AccountClient acct_client;
+
+    google::protobuf::StringValue username;
+    username.set_value( msg.to() );
+
+    account::User user;
+    ret = acct_client.Get( username, &user );
+
+    if( 0 != ret ) return -2;
+
+    // TODO: check req_obj.from is friend of req_obj.to
+
+    msgbox::MsgIndex index;
+
+    SeqClient seq_client;
+    {
+        seq::AllocReq seq_req;
+        google::protobuf::UInt32Value seq_resp;
+
+        seq_req.set_username( head.username() );
+        seq_req.set_type( seq::TYPE_MSG );
+
+        seq_client.Alloc( seq_req, &seq_resp );
+
+        index.set_seq( seq_resp.value() );
+    }
+
+    // fill index by msg
+    index.set_from( head.username() );
+    index.set_to( msg.to() );
+    index.set_content( msg.content() );
+    index.set_createtime( msg.createtime() );
+    index.set_uuid( msg.uuid() );
+
+    MsgBoxClient msgbox_client;
+
+    return msgbox_client.Add( index, result );
 }
 
 int CgiSendMsg :: Process( const logic::ReqHead & head,
@@ -23,40 +69,14 @@ int CgiSendMsg :: Process( const logic::ReqHead & head,
     logic::SendMsgResponse resp_obj;
 
     req_obj.ParseFromString( req_buff );
-
-    // TODO: check req_obj.from is friend of req_obj.to
-
-    MsgBoxClient msgbox_client;
-
+ 
     for( int i = 0; i < req_obj.msg_size(); i++ ) {
         msgbox::MsgIndex index;
         msgbox::AddMsgResp result;
 
         const logic::MsgRequest & msg = req_obj.msg(i);
 
-        SeqClient seq_client;
-        {
-            seq::AllocReq seq_req;
-            google::protobuf::UInt32Value seq_resp;
-
-            seq_req.set_username( head.username() );
-            seq_req.set_type( seq::TYPE_MSG );
-
-            seq_client.Alloc( seq_req, &seq_resp );
-
-            index.set_seq( seq_resp.value() );
-        }
-
-        // fill index by msg
-        index.set_from( head.username() );
-        index.set_to( msg.to() );
-        index.set_content( msg.content() );
-        index.set_createtime( msg.createtime() );
-        index.set_uuid( msg.uuid() );
-
-        int ret = msgbox_client.Add( index, &result );
-
-        printf( "AddMsg %d\n", ret );
+        int ret = AddOneMsg( head, msg, &result );
 
         // append result to resp_obj
         logic::MsgResponse * resp_msg = resp_obj.add_msg();
