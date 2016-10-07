12868cee4c67539fe98e871668f15accb9894638
xiaobinshe <xiaobinshe@gmail.com>
2016-10-07 13:21:47 +0800

    modify bm tools

--- minichat/tools/benchmark.h
+++ minichat/tools/benchmark.h
@@ -2,6 +2,4 @@
 #pragma once
 
 extern int benchmark( int begin_index, int thread_count,
-        int uthread_per_thread, int msg_per_user,
-        int begin_qps_per_uthread, int max_qps_per_uthread, int qps_time_interval_per_uthread,
-        int qps_increment_per_uthread  );
+        int uthread_per_thread, int msg_per_user );
--- minichat/tools/bm_tools.cpp
+++ minichat/tools/bm_tools.cpp
@@ -13,22 +13,14 @@ void run( phxrpc::OptMap & opt_map )
     int uthread_per_thread = atoi( opt_map.Get( 'u' ) );
     int begin_index = atoi( opt_map.Get( 'b' ) );
     int msg_per_user = atoi( opt_map.Get( 'm' ) );
-    int begin_qps_per_uthread = atoi( opt_map.Get( 'q' ) );
-    int max_qps_per_uthread = atoi( opt_map.Get( 'x' ) );
-    int qps_time_interval_per_uthread = atoi( opt_map.Get( 'i' ) );
-    int qps_increment_per_uthread = atoi( opt_map.Get( 'n' ) );
 
-    benchmark( begin_index, thread_count, uthread_per_thread, msg_per_user,
-           begin_qps_per_uthread, max_qps_per_uthread, qps_time_interval_per_uthread,
-          qps_increment_per_uthread );
+    benchmark( begin_index, thread_count, uthread_per_thread, msg_per_user );
 }
 
 void ShowUsage( const char * program )
 {
     printf( "\nUsage: %s [-t <thread>] [-u <uthread per thread>] "
-            "          [-b <begin user index>] [-m <msg per user>]\n"
-            "          [-q begin qps] [-x max qps] [-i qps time interval] [-n qps increment] \n",
-            program );
+            "[-b <begin user index>] [-m <msg per user>]\n", program );
     printf( "\n" );
     printf( "\t%d users per uthread/channel as default\n", PushClient::USER_PER_CHANNEL );
     printf( "\n" );
@@ -40,7 +32,7 @@ int main( int argc, char * argv[] )
 {
     phxrpc::openlog( argv[0], "~/log/error", LOG_DEBUG);
 
-    phxrpc::OptMap opt_map( "q:x:i:n:t:u:b:m:v" );
+    phxrpc::OptMap opt_map( "t:u:b:m:v" );
 
     if( ! opt_map.Parse( argc, argv ) ) ShowUsage( argv[0] );
 
